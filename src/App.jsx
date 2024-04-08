0a59b399efd0c61dfe5fbd3a0cae6413eae0c4b0
sirojUzb <sirojiddinmamatovuzb@gmail.com>
2024-04-08 21:38:19 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Component } from "react";
-import Lesson3 from "./Lesson3";
-// import Lesson3part2 from "./Lesson3part2";
+// import Lesson3 from "./Lesson3";
+import Lesson3part2 from "./Lesson3part2";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Lesson3 />
-        {/* <Lesson3part2 /> */}
+        {/* <Lesson3 /> */}
+        <Lesson3part2 />
       </div>
     );
   }
