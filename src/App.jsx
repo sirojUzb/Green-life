bbe73a733d1097775c6853d75ca4ebab16cc8455
sirojUzb <sirojiddinmamatovuzb@gmail.com>
2024-04-13 03:56:30 +0900

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Component } from "react";
 // import Lesson3 from "./Lesson3";
-import Lesson3part2 from "./Lesson3part2";
-import Lesson2 from "../lesson2";
+// import Lesson3part2 from "./Lesson3part2";
+// import Lesson2 from "./lesson2";
+import Lesson2part1 from "./Lesson2part1";
 
 class App extends Component {
   render() {
     return (
       <div>
         {/* <Lesson3 /> */}
-        <Lesson3part2 />
-        <Lesson2 />
+        {/* <Lesson3part2 /> */}
+        {/* <Lesson2 /> */}
+        <Lesson2part1 />
       </div>
     );
   }
---
+++ src/Lesson2part1/index.jsx
@@ -0,0 +1,13 @@
+// Decloration
+function Function_Dec() {
+  return <div>Hello (Decloration component)</div>;
+}
+// // Expression
+// const Function_Exp = function () {
+//   return <div>Hello(Expression component)</div>;
+// };
+// // Arrow
+// const Function_Arr = () => {
+//   return <div>Hello(Arrow component)</div>;
+// };
+export default Function_Dec;
