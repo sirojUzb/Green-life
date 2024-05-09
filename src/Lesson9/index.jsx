3def5fc90c0bb5a5e9ad9c85cad1e99a8ebfd746
sirojUzb <sirojiddinmamatovuzb@gmail.com>
2024-05-09 21:59:02 +0900

    les9

--- src/Lesson9/index.jsx
+++ src/Lesson9/index.jsx
@@ -1,10 +1,10 @@
-const Lesson9 = () => {
+function Lesson9() {
   return <div>Lesson9</div>;
-};
+}
 
 export default Lesson9;
 // STATE => FUNCTIONAL COMPONENT
 // USE = hook deb ham ishlatiladi:
 // useEffect, usestate, useReducer
 // effectHook, stateHook, reducerHook.
-//
+// finish it on Thursday
